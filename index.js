39098a5261b8a5ad87ca2856b7de8b9555ca77d4
Zahra Khan <zarssskhan@gmail.com>
2017-09-19 03:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
-    
+    if (findFn(array[i])) {
+      return array[i];
+    }
   }
 }
