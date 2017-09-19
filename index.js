c160c42f40bfdf1548af308e6606315f4f9e9ae7
Zahra Khan <zarssskhan@gmail.com>
2017-09-19 03:22:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function breakOut(array, changeValue, stopValue) {
   for(var i = 0; i < array.length; i++) {
     if (array[i] === stopValue) {
       break;
-    } 
+    }
+
+    array[i] = changeValue;
   }
 }
