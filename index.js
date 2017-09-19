7ac3fb5e678925cf9622de22e62357bc7188d397
Zahra Khan <zarssskhan@gmail.com>
2017-09-19 03:24:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ function breakOut(array, changeValue, stopValue) {
 
 
 function keepGoing(array, changeValue, skipValue) {
-  
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== skipValue) {
+      continue;
+    }
+  }
 }
